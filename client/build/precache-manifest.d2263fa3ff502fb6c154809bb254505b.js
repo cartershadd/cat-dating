ea6a4b697d74eeab78f079ea8ddf290095415c12
Carter Shadden <carter.shadden@gmail.com>
2019-09-04 14:30:11 -0400

    deployment

---
+++ client/build/precache-manifest.d2263fa3ff502fb6c154809bb254505b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208a3005b038250def58c9c792b6cbfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbafb84251ea6dfc4028",
+    "url": "/static/css/main.0b16e6bc.chunk.css"
+  },
+  {
+    "revision": "1d5419b85e7f3be40d2d",
+    "url": "/static/js/2.f0be9da9.chunk.js"
+  },
+  {
+    "revision": "dbafb84251ea6dfc4028",
+    "url": "/static/js/main.ef2852e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
